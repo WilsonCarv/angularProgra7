23daa7b7be9c3c6b4b0cb28d72ab15bb52ab9422
WilsonCarv <alfaro9331qgmail.com>
2019-08-05 14:29:26 -0600

    guardar expedinte errores

--- src/app/perfil/expediente/expediente.component.ts
+++ src/app/perfil/expediente/expediente.component.ts
@@ -26,6 +26,7 @@ export class ExpedienteComponent implements OnInit {
   name = "Angular";
   fuma = true;
   toma = true;
+  expediente: ExpedienteEntidad;
   alergiasFrec: Alergiafrec;
   selectedAlergias: Alergiafrec;
   actividadesFrecuentes: ActividadesFisicas;
@@ -42,17 +43,20 @@ export class ExpedienteComponent implements OnInit {
     observaciones: string;
   }>;
   error: any;
+  datos: Expediente;
   private stepper: Stepper;
   constructor(
     private router: Router,
     private alergiaFrecuenteService: AlergiafrecService,
     private actividadFrecuenteService: ActividadesFisicasService,
-    private notificacion: NotificacionService
+    private notificacion: NotificacionService,
+    private ExpedienteServ: ExpedienteService
   ) {
     this.getAlergias();
     this.getActividades();
     this.actividades = [];
     this.alergias = [];
+
   }
 
   getAlergias() {
@@ -92,7 +96,7 @@ export class ExpedienteComponent implements OnInit {
     };
     this.actividades.push(arr);
     //Codigo para Limpiar el Form
-    (<HTMLFormElement>document.getElementById("otrasActividades")).reset();
+    (<HTMLFormElement> document.getElementById("otrasActividades")).reset();
   }
 
   addArrayAlergias(event, otrasForm: NgForm) {
@@ -108,7 +112,7 @@ export class ExpedienteComponent implements OnInit {
     };
     this.alergias.push(arr);
     //Codigo para Limpiar el Form
-    (<HTMLFormElement>document.getElementById("otrasAlerigas")).reset();
+    (<HTMLFormElement> document.getElementById("otrasAlerigas")).reset();
   }
 
   eliminarActividad(doc) {
@@ -127,14 +131,59 @@ export class ExpedienteComponent implements OnInit {
     }
   }
 
-  onSubmit(obj: ExpedienteEntidad) {
-    /* console.log("Segundo Apellido", event.target.SegundoApellido.value);
-    console.log("Sexo", event.target.sexo.value);
-    console.log("Fecha", event.target.fechaNacimiento.value);
-    console.log("Alergias", event.target.plataformas_id);
-    console.log("Actividades Selecionanadas", this.selectedActividades);
-    console.log("Alergias Selecionanadas", this.selectedAlergias);
-    return false;*/
+  onSubmit(event) {
+    this.expediente = new ExpedienteEntidad();
+
+   this.expediente.identificacion = event.target.Identificacion.value;
+   this.expediente.nombre = event.target.Nombre.value;
+   this.expediente.primer_apellido = event.target.PrimerApellido.value;
+   this.expediente.segundo_apellido = event.target.SegundoApellido.value;
+   this.expediente.sexo = event.target.sexo.value;
+   this.expediente.fecha_nacimiento = event.target.fechaNacimiento.value;
+   this.expediente.tipo_sangre = event.target.tipoSangre.value;
+   this.expediente.lugar_Residencia = event.target.lugarResidencia.value;
+   this.expediente.numero_telefono = event.target.numeroTelefono.value;
+   this.expediente.contacto_emergencia = event.target.contactoEmergencia.value;
+   if (event.target.customSwitch1.value === true) {
+      this.expediente.fumado = 1;
+      this.expediente.historial_fumado = event.target.historialFumando.value;
+      this.expediente.cantidad_cigarros_dia = event.target.cantidaCigarros.value;
+      this.expediente.edad_comenzo_fumar = event.target.edadComenzoFumar.value;
+      this.expediente.observaciones_fumado = event.target.observacionesFumado.value;
+    } else {
+      this.expediente.fumado = 0;
+    }
+   if (event.target.customSwitch2.value === true) {
+      this.expediente.alcohol = 1;
+      this.expediente.historial_alcohol = event.target.historialAlcohol.value;
+      this.expediente.edad_comenzo_tomar = event.target.edadComenzoTomar.value;
+      this.expediente.frecuencia_alcohol = event.target.cantidaToma.value;
+      this.expediente.toma_usualmente = event.target.tipoAlcohol.value;
+      this.expediente.observaciones_alcohol = event.target.observacionesAlchohol.value;
+    } else {
+      this.expediente.alcohol = 0;
+    }
+   console.log("Segundo Apellido", event.target.SegundoApellido.value);
+   console.log("Sexo", event.target.sexo.value);
+   console.log("Fecha", event.target.fechaNacimiento.value);
+   console.log("Alergias", event.target.plataformas_id);
+   console.log("Actividades Selecionanadas", this.selectedActividades);
+   console.log("Alergias Selecionanadas", this.selectedAlergias);
+
+   return this.ExpedienteServ.createExpediente(this.expediente).subscribe(
+      (respuesta: Expediente) => {
+        this.datos = respuesta;
+        this.router.navigate(['/'], {
+          queryParams: { create: 'true' }
+        });
+      },
+      error => {
+        this.error = error;
+        this.notificacion.msjValidacion(this.error);
+      }
+    );
+
+
   }
 
   changeStatusFuma(event) {
