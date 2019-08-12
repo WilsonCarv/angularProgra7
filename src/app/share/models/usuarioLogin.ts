a1739421b964beea395d434f88ba7bac7c0caa99
WilsonCarv <alfaro9331qgmail.com>
2019-08-11 23:08:05 -0600

    a

--- src/app/share/models/usuarioLogin.ts
+++ src/app/share/models/usuarioLogin.ts
@@ -2,6 +2,7 @@ import { UserEntidad } from './user-entidad';
 
 export interface UsuarioLogin {
   access_token: string;
+  msg: string;
   user: UserEntidad;
   token_type: string;
   expires_in: number;
